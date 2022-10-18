90a2d00614839b959c55d8a5821d00fe0d7f455b
Zigijos <zigijos@gmail.com>
2022-10-18 13:26:43 -0700

    5-sign commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,11 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
-		return ('1');
+		printf('+1');
 	}
 	else if (n == 0)
 	{
-		putchar('0');
-		return ('0');
+		printf('00');
 	}
-	putchar('-');
-	return ('1');
+	printf('-1');
 }
