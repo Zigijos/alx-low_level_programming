930d7525d189d0da6dc76dfa34bf80009f50dda6
Zigijos <zigijos@gmail.com>
2022-10-18 12:49:59 -0700

    5-sign commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - is a function that prints the sign of a number.
+ *
+ * @n: is the number to be check
+ *
+ * Return: Returns 1 if n > 0, Returns 0 if n = 0, Returns -1 if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		printf('+');
+		return (1);
+	else if (n == 0)
+		printf('0');
+		return (0);
+	printf('-');
+	return (-1);
+}
