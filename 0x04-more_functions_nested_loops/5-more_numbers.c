03b3c6b15b9f1f6d1b0aba3d5ad561041712d54e
Zigijos <zigijos@gmail.com>
2022-10-22 15:54:47 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,10 +9,10 @@ void more_numbers(void)
 
 {
 	int num, count;
-	
+
 	for (count = 0; count <= 9; count++)
 	{
-		for (num = 0; num <= 14; num);
+		for (num = 0; num <= 14; num)
 		{
 			if (num > 9)
 				putchar((num / 10) + '0');
