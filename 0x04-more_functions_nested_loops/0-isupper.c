22b8575e4452c6e9ed538e037804e0ceff8144dc
Zigijos <zigijos@gmail.com>
2022-10-22 07:47:08 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,27 @@
  * Return: 0 or 1
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
+}
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
