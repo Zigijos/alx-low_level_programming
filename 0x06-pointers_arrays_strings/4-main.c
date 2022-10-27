86c1d841f312eccfd405ba04f4f7cb592233f3ef
Zigijos <zigijos@gmail.com>
2022-10-27 14:56:18 +0000

    commit

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * print_array - check the code
  * @a: an array of integers
  * @n: the number of elements to swap
  *
@@ -12,9 +12,9 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	i = 0;
-	
+
 	while (i < n)
 	{
 		if (i != 0)
@@ -26,7 +26,6 @@ void print_array(int *a, int n)
 	}
 	printf("\n");
 }
-
 /**
  * main - check the code
  *
@@ -36,7 +35,7 @@ void print_array(int *a, int n)
 int main(void)
 {
 	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-	
+
 	print_array(a, sizeof(a) / sizeof(int));
 	reverse_array(a, sizeof(a) / sizeof(int));
 	print_array(a, sizeof(a) / sizeof(int));
