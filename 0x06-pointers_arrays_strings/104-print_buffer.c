c7acaa93ed6db0d02fd7ac7fbab61c427cce935d
Zigijos <zigijos@gmail.com>
2022-10-27 18:36:38 +0000

    commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,7 +12,7 @@
 void print_buffer(char *b, int size)
 {
 	int byte, index;
-	
+
 	for (byte = 0; byte < size; byte += 10)
 	{
 		printf("%08x: ", byte);
@@ -25,19 +25,19 @@ void print_buffer(char *b, int size)
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
 		}
-	       for (index = 0; index < 10; index++)
-	       {
-		       if ((index + byte) >= size)
-			       break;
-		       else if (*(b + index + byte) >= 31 &&
-				       *(b + index + byte) <= 126)
-			       printf("%c", *(b + index + byte));
-		       else
-			       printf(".");
-	       }
-	       if (byte >= size)
-		       continue;
-	       printf("\n");
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+			else if (*(b + index + byte) >= 31 &&
+					*(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+			else
+				printf(".");
+		}
+		if (byte >= size)
+			continue;
+		printf("\n");
 	}
 	if (size <= 0)
 		printf("\n");
