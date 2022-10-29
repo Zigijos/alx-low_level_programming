8b87c5065d6a7dd85cc7ea9460f31c7cc02d215f
Zigijos <zigijos@gmail.com>
2022-10-29 14:33:55 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,10 +13,10 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar(45);
-		num = -n;
+		_putchar('-');
+		num = -num;
 	}
-	if (num / 10)
+	if ((num / 10) > 0)
 		print_number(num / 10);
-	putchar((num % 10) + '0');
+	_putchar((num % 10) + '0');
 }
