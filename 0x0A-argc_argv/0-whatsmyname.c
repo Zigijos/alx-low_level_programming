63653cdab288a21edff717319e19896af0e2a99e
Zigijos <zigijos@gmail.com>
2022-11-07 14:21:09 +0000

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * @argv: argument string
  * Return: always 0
  */
-int main(int argc, char *argv[])
-
-{	(void) argc;
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	(void) argc;
 	printf("%s\n,", argv[0]);
 	return (0);
 }
