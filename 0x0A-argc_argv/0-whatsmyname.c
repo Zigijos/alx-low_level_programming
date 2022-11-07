bdbf27599cff22a8906cf012bdd8d3d0eb1670d8
Zigijos <zigijos@gmail.com>
2022-11-07 13:32:59 +0000

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name of program, followed by new name
+ * @argc: argument count
+ * @argv: argument string
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n,", argv[i]);
+	return (0);
+}
