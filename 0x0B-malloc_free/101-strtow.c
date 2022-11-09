b72112757bcb6458878a8ec6d5798170b29fa067
Zigijos <zigijos@gmail.com>
2022-11-09 23:59:31 +0000

    commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,29 +1,114 @@
 #include "main.h"
 #include <stdlib.h>
 
-int word_len(char *str);
-int count_words(char *str);
-char **strtow(char *str);
+void util(char **, char *);
+void create_word(char **, char *, int, int, int);
 
 /**
- * strtow - returns a pointer to an array
- * word_len - Locates the index marking the end of the
- *            first word contained within a string.
- *
- * @str: The string to be searched.
- * Return: The index marking the end of the initial word pointed to by str.
+ * strtow - splits a string into words.
+ * @str: the string
+ * Return: returns a pointer to an array of strings (words)
  */
 
 char **strtow(char *str)
 {
-	int index = 0, len = 0;
+	int i, flag, len;
 
-	while (*(str + index) && *(str + index) != ' ')
+	char **words;
+
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == ' \0'))
+
+		return (NULL);
+
+	i = flag = len = 0;
+
+	while (str[i])
 	{
-		len++;
+		if (flag == 0 && str[i] != ' ')
 
-		index++;
+			flag = 1;
+
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			flag = 0;
+			len++;
+		}
+
+		i++;
 	}
-	return (len);
-}
+	len += flag == 1 ? 1 : 0;
 
+	if (len == 0)
+
+		return (NULL);
+
+	words = (char **)malloc(sizeof(char *) * (len + 1));
+
+	if (words == NULL)
+
+		return (NULL);
+
+	util(words, str);
+
+	words[len] = NULL;
+
+	return (words);
+}
+/**
+ * util - a util function for fetching words into an array
+ * @words: the strings array
+ * @str: the string
+ */
+
+void util(char **words, char *str)
+{
+	int i, j, start, flag;
+
+	i = j = flag = 0;
+
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+		{
+			start = i;
+
+			flag = 1;
+		}
+
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			create_word(words, str, start, i, j);
+
+			j++;
+
+			flag = 0;
+		}
+		i++;
+	}
+	if (flag == 1)
+
+		create_word(words, str, start, i, j);
+}
+/**
+ * create_word - creates a word and insert it into the array
+ * @words: the array of strings
+ * @str: the string
+ * @start: the starting index of the word
+ * @end: the stopping index of the word
+ * @index: the index of the array to insert the word
+ */
+
+void create_word(char **words, char *str, int start, int end, int index)
+{
+	int i, j;
+
+	i = end - start;
+
+	words[index] = (char *)malloc(sizeof(char) * (i + 1));
+
+	for (j = 0; start < end; start++, j++)
+
+		words[index][j] = str[start];
+
+	words[index][j] = '\0';
+}
