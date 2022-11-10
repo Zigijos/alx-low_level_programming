c13bf110f0238d97d2daf3c9674e0f24fa243222
Zigijos <zigijos@gmail.com>
2022-11-10 00:15:14 +0000

    commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,7 +16,7 @@ char **strtow(char *str)
 
 	char **words;
 
-	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == ' \0'))
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 
 		return (NULL);
 
