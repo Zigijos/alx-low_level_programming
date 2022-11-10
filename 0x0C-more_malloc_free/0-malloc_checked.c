a44c74654bff90a6729ae9905ae8e2d71270c2d0
Zigijos <zigijos@gmail.com>
2022-11-10 22:31:48 +0000

    commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
+ * Return: A pointer to the allocated memory.
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	void *mem_alloc = malloc(b);
+
+	if (mem_alloc == NULL)
+	{
+		exit(98);
+	}
+	return (mem_alloc);
+}
