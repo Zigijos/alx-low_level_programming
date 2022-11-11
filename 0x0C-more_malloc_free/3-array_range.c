7f46685fb0008f925f77084557726a796a761833
Zigijos <zigijos@gmail.com>
2022-11-11 00:53:49 +0000

    commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ * array_range - ...
+ * @min: ...
+ * @max: ...
+ * Return: integer value
+ */
 
 int *array_range(int min, int max)
 {
