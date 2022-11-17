5024646c9e473c2c729344ac82bae12c48ef44cb
Zigijos <zigijos@gmail.com>
2022-11-17 20:13:53 +0000

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,19 +11,16 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int index;
 
-	if (size > 0)
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
 	{
-		if (array != NULL && cmp != NULL)
-		{
-			while (i < size)
-			{
-				if (cmp(array[i]))
-					return (i);
-				i++;
-			}
-		}
+		if (cmp(array[index]) != 0)
+
+			return (index);
 	}
 	return (-1);
 }
